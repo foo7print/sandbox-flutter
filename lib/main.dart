fbf0bc23b0349447b95ed9e688e164a6ca9ca51c
foo7print <foo7print@gmail.com>
2023-08-07 12:45:24 +0900

    modify body Column Widget

--- lib/main.dart
+++ lib/main.dart
@@ -49,10 +49,12 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'テキストテキストテキストテキストテキストテキスト',
+            ),
+            const Text(
+                'あいうえおかきくけこさしすせそ'
             ),
             Text(
               '$_counter',
